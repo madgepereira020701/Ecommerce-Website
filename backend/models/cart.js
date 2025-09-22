8b1891f219a4950b939035f66710ad4908190b59
madgepereira020701 <madgepereira020701@gmail.com>
2025-09-22 11:46:07 +0530

    Added cart.js in model

---
+++ backend/models/cart.js
@@ -0,0 +1,28 @@
+// models/Cart.js
+const mongoose = require("mongoose");
+
+const cartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    products: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        name: String,
+        price: Number,
+        image: String,
+        quantity: { type: Number, default: 1 },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Cart", cartSchema);
